e2857fb3b8ab7041e7684eda5d7bfa0aa36aa147
elviawu <elviawu@gmail.com>
2023-04-21 17:21:33 +0800

    feat: add user registration feature

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const userSchema = new Schema {
+const userSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -14,8 +14,8 @@ const userSchema = new Schema {
     required: true
   },
   createdAt: {
-    type: date,
-    required: Date.now
-  },
-}
+    type: Date,
+    default: Date.now
+  }
+})
 module.exports = mongoose.model('User', userSchema)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 // 引用epress和express路由器
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,6 +12,33 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body
+  // 如果已經註冊：退回原本畫面
+  User.findOne({ email }).then(user => {
+    if(user) {
+      console.log('user already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password,
+      })
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+    }
+  })
+  .catch(error => console.log(error))
 })
 
 module.exports = router
